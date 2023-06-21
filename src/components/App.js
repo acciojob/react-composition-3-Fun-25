a96de3b15b14d31fe5c7dbbc289d218d97a24c4b
Fun-25 <muskandodmani760@gmail.com>
2023-06-21 20:40:32 +0530

    submitting code-Wed Jun 21 2023 20:40:28 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
 
+import { useState } from 'react';
 import React from "react";
 import './../styles/App.css';
-
+ 
 const App = () => {
+  const [hover, setHover] = useState({ id: 0, status: false });
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Tooltip hover={hover} setHoverEffect={setHover} />
     </div>
-  )
+  );
+};
+
+function Tooltip({ hover, setHoverEffect }) {
+  function handleHover(id) {
+    setHoverEffect({ id: id, status: true });
+  }
+  return (
+    <div>
+      <div className="tooltiptext">
+        {hover.id == 1 && <div>This is a tooltip</div>}
+        <h1 onMouseOver={() => handleHover(1)}>Hover over me</h1>
+      </div>
+      <hr />
+      <div className="tootip">
+        {hover.id == 2 && <div>This is another tooltip</div>}
+        <p onMouseOver={() => handleHover(2)}>
+          Hover over me to see another tooltip
+        </p>
+      </div>
+      <hr />
+    </div>
+  );
 }
 
 export default App
